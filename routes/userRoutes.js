dd993c6296a06cd254f51cd3831b58bfc57e53fd
Mohan <veerubhotla.mohan@gmail.com>
2022-07-12 15:13:22 +0530

    User feature added

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,5 +3,6 @@ const { signup } = require('../controllers/userController');
 const router = express.Router();
 
 router.post('/signup', signup)
+router.post('/login', signup)
 
 module.exports = router
